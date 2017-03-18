d6b91ee5bdf687c05a64a596a5b5a0355ca60442
digiholic <digikun@gmail.com>
2017-03-18 08:01:30 -0600

    Added NGUI Free and created a few menus

---
+++ Assets/Menu/MenuColorSetter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class is going to involve hard coding some assumptions about what color you're setting to save on code.
+/// </summary>
+public class MenuColorSetter : MonoBehaviour {
+    private MenuColorChanger menu_color;
+
+    private UISprite ui_sprite;
+    private UILabel ui_label;
+    private UIButton ui_button;
+
+	// Use this for initialization
+	void Start () {
+        menu_color = GameObject.FindObjectOfType<MenuColorChanger>();
+
+        ui_sprite = GetComponent<UISprite>();
+        ui_label = GetComponent<UILabel>();
+        ui_button = GetComponent<UIButton>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (ui_sprite != null) //Sprites that have this component get their color set
+        {
+            ui_sprite.color = menu_color.getColor();
+        }
+        if (ui_button != null) //Buttons that have this component have their hover color set
+        {
+            
+            ui_button.hover = menu_color.getColor();
+        }
+	}
+}
---
+++ Assets/MenuColorChanger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuColorChanger : MonoBehaviour {
+
+    public float hue = 0.0f; //hue hue hue
+    public Color hsvColor = Color.black;
+
+    
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        hue = (hue + 0.05f * Time.deltaTime) % 1.0f;
+        hsvColor = Color.HSVToRGB(hue, 0.8f, 1.0f);
+    }
+
+    public Color getColor()
+    {
+        return hsvColor;
+    }
+}
