62c21eaad8c8dd5e9fd327125df2e5b0d27d69a0
digiholic <digikun@gmail.com>
2018-06-25 08:45:38 -0600

    Begun adding new assets to a new builder layout

---
+++ Assets/Menu/NewLegacyEditor/LegacyEditorData.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LegacyEditorData : MonoBehaviour {
+    public FighterInfo loadedFighter;
+    public ActionFile loadedActionFile;
+    public GameAction currentAction;
+    public BuilderLeftDropdown leftDropdown;
+    public BuilderRightDropdown rightDropdown;
+    public int currentFrame;
+    public BuilderSelectable currentSelected;
+    public string contextFighterCategory;
+    public string contextSubactionCategory;
+    public string actionSearchText;
+    public string spriteSearchText;
+    public SortOrder sortOrder;
+}
+
+public enum BuilderLeftDropdown
+{
+    ACTION,
+    SPRITE
+}
+
+public enum BuilderRightDropdown
+{
+    PROPERTIES
+}
+
+public enum SortOrder
+{
+    NAME,
+    CATEGORY
+}
