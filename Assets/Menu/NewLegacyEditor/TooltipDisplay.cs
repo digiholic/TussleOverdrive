ddf7693ea592264626e7956009faf792755401d9
digiholic <digikun@gmail.com>
2019-01-26 21:20:13 -0700

    Added tooltips to the bottom of the builder for most objects

---
+++ Assets/Menu/NewLegacyEditor/TooltipDisplay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TooltipDisplay : MonoBehaviour
+{
+    private static TooltipDisplay instance;
+    private UILabel text;
+
+    private void Awake()
+    {
+        instance = this;
+        text = GetComponent<UILabel>();
+    }
+
+    private void Update()
+    {
+        if (UICamera.hoveredObject == null)
+        {
+            DisplayTooltip("");
+        }
+    }
+
+    public static void DisplayTooltip(string tooltip)
+    {
+        tooltip = tooltip.Replace("\n", " ").Replace("\r", " ");
+        instance.text.text = tooltip;
+    }
+}
---
+++ Assets/Menu/NewLegacyEditor/TooltipSubactionCreateButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TooltipSubactionCreateButton : MonoBehaviour
+{
+    private SubactionCreateButton button;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        button = GetComponent<SubactionCreateButton>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (UICamera.hoveredObject == gameObject)
+        {
+            TooltipDisplay.DisplayTooltip(button.subData.Description);
+        }
+    }
+}
