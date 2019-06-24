060a6db698961cab6e53e8633d6231b88f5e0a6a
digiholic <digikun@gmail.com>
2019-06-23 21:42:52 -0600

    Added Loop to Animation Properties

--- Assets/Menu/LegacyEditor/Panels/RightPanel/AnimationPropertiesInput.cs
+++ Assets/Menu/LegacyEditor/Panels/RightPanel/AnimationPropertiesInput.cs
@@ -11,25 +11,43 @@ public class AnimationPropertiesInput : LegacyEditorWidget
     private VarType varType;
 
     private UIInput input;
+    private UIToggle toggle;
 
     private void Awake()
     {
         input = GetComponent<UIInput>();
+        toggle = GetComponent<UIToggle>();
     }
 
     public void SendAction(object propertyVal)
     {
         ModifyAnimationEditorAction legacyAction = ScriptableObject.CreateInstance<ModifyAnimationEditorAction>();
         int propNum = 0;
+        float propFloat = 0.0f;
         if (varType == VarType.INT)
         {
+            if (propertyVal == null) propertyVal = "0";
             if (int.TryParse(propertyVal.ToString(), out propNum))
             {
                 legacyAction.init(propertyName, propNum);
             }
         }
+        else if (varType == VarType.FLOAT)
+        {
+            if (propertyVal == null) propertyVal = "0.0";
+            if (float.TryParse(propertyVal.ToString(), out propFloat))
+            {
+                legacyAction.init(propertyName, propFloat);
+            }
+        }
+        else if (varType == VarType.BOOL)
+        {
+            if (propertyVal == null) propertyVal = "false";
+            legacyAction.init(propertyName, (bool)propertyVal);
+        }
         else
         {
+            if (propertyVal == null) propertyVal = "";
             legacyAction.init(propertyName, propertyVal.ToString());
         }
         LegacyEditorData.instance.DoAction(legacyAction);
@@ -39,14 +57,27 @@ public class AnimationPropertiesInput : LegacyEditorWidget
     {
         if (def != null && def.AnimationName != "")
         {
-            input.value = GetProperty(def);
+            if (input != null)
+            {
+                input.value = GetStringProperty(def);
+            }
+            else if (toggle != null)
+            {
+                toggle.value = GetBoolProperty(def);
+            }
         }
     }
 
-    private string GetProperty(AnimationDefinition def)
+    private string GetStringProperty(AnimationDefinition def)
     {
         PropertyInfo prop = def.GetType().GetProperty(propertyName);
         return prop.GetValue(def).ToString();
+    }
+
+    private bool GetBoolProperty(AnimationDefinition def)
+    {
+        PropertyInfo prop = def.GetType().GetProperty(propertyName);
+        return (bool) prop.GetValue(def);
     }
 
     public override void RegisterListeners()
