82805e5698801252df417cb194d4314042cd9350
digiholic <digikun@gmail.com>
2020-01-01 17:01:38 -0700

    Added a post-build hook to copy over modules and settings as needed

---
+++ Assets/Editor/PostBuild.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using System.IO;
+using UnityEditor.Build;
+using UnityEngine;
+using UnityEditor.Build.Reporting;
+
+class PostBuild : IPostprocessBuildWithReport
+{
+    public int callbackOrder { get { return 0; } }
+    public void OnPostprocessBuild(BuildReport report)
+    {
+        Debug.Log(report.summary.outputPath);
+        FileInfo info = new FileInfo(report.summary.outputPath);
+        DirectoryInfo buildTargetDirectory = info.Directory;
+        DirectoryInfo buildAssetsDirectory = buildTargetDirectory.CreateSubdirectory("Assets");
+        DirectoryInfo buildModulesDirectory = buildAssetsDirectory.CreateSubdirectory("Modules");
+
+        FileLoader.CopyDirectory(FileLoader.ModulePath,buildModulesDirectory.FullName,true);
+
+        DirectoryInfo buildSettingsDirectory = buildAssetsDirectory.CreateSubdirectory("Settings");
+
+        FileLoader.CopyDirectory(FileLoader.SettingsPath,buildSettingsDirectory.FullName,true);
+
+        Debug.Log("Copied Modules Directory");
+    }
+}
