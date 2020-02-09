d09d1a27ffb4f6a10f0f9bc5eedf531b4d5cc77f
digiholic <digikun@gmail.com>
2020-02-08 17:24:03 -0700

    Added -editor flag to boot the game directly into the editor

---
+++ Assets/Engine/CommandLineParser.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CommandLineParser : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string[] args = System.Environment.GetCommandLineArgs ();
+        for (int i = 0; i < args.Length; i++) {
+            //If this is an editor command, go straight there
+            if (args [i] == "-editor") {
+                SceneManager.LoadScene("LegacyEditor", LoadSceneMode.Single);
+            }
+        }
+    }
+}
